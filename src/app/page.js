00c66d32db565a04f0ef1087885893e8e288b942
Huy Huy Hoang <hoang18769@gmail.com>
2025-06-22 22:21:41 +0700

    fix route / redirect

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,23 @@
+import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export default function RootPage() {
-  redirect('/index')
+  // Get cookies on server side
+  const cookieStore = cookies()
+  const token = cookieStore.get('accessToken')?.value || cookieStore.get('token')?.value
+  const userId = cookieStore.get('userId')?.value
+  
+  console.log('🏠 Root page check:', {
+    hasToken: !!token,
+    hasUserId: !!userId
+  })
+  
+  // Check authentication and redirect accordingly
+  if (token && userId) {
+    // User is authenticated, redirect to index
+    redirect('/index')
+  } else {
+    // User is not authenticated, redirect to register
+    redirect('/register')
+  }
 }
