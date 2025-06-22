c1ea332e61cd63f295914cc480e54b0d524ad69f
Huy Huy Hoang <hoang18769@gmail.com>
2025-06-22 18:36:02 +0700

    fix eslint dirname

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+// eslint.config.js
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const compat = new FlatCompat();
+export default [...compat.extends("next/core-web-vitals")];
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-export default eslintConfig;
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+// export default eslintConfig;
+
